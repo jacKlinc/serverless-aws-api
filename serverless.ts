bd7234c9eba9f6761861e182925c013fde1f1e07
jack143 <jack@inne.io>
2023-01-19 16:11:46 +0100

    Changed to esbuild and node16 Added hello world Dynamo table

--- serverless.ts
+++ serverless.ts
@@ -1,32 +1,57 @@
 import 'dotenv/config';
-import type { AWS } from "@serverless/typescript";
+import type { AWS } from '@serverless/typescript';
 
-import getCityInfo from "@functions/getCityInfo";
+import getCityInfo from '@functions/getCityInfo';
+import dynamoResources from './src/dynamoResources';
 
 const serverlessConfiguration: AWS = {
-  service: "serverless-aws-api",
-  frameworkVersion: "3",
+  service: 'serverless-aws-api',
+  frameworkVersion: '3',
   custom: {
-    webpack: {
-      webpackConfig: "./webpack.config.js",
-      includeModules: true,
+    myTable: '${sls:stage}-my-table',
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
     },
   },
-  plugins: ["serverless-webpack"],
+  plugins: ['serverless-esbuild'],
   provider: {
-    name: "aws",
-    runtime: "nodejs14.x",
+    name: 'aws',
+    runtime: 'nodejs16.x',
     region: 'eu-central-1',
+    // profile: 'jacKlinc',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      myTable: '${self:custom.myTable}',
     },
-    lambdaHashingVersion: "20201221",
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 'dynamodb:*',
+        Resource: [
+          'arn:aws:dynamodb:${self:provider.region}:${aws:accountId}:table/${self:custom.myTable}',
+        ],
+      },
+    ],
   },
   functions: { getCityInfo },
+  resources: {
+    Resources: {
+      ...dynamoResources,
+    },
+  },
+  package: { individually: true },
 };
 
 module.exports = serverlessConfiguration;
